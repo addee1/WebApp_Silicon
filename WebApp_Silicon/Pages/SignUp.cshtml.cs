1083ce929c39fb17d7e55b5a170ec044f40e5222
Adam Ottosson <addeecsgo@gmail.com>
2024-02-26 18:08:53 +0100

    Sign up completed. Checkbox must be styled..

--- WebApp_Silicon/Pages/SignUp.cshtml.cs
+++ WebApp_Silicon/Pages/SignUp.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp_Silicon.Models;
 
-namespace WebApp_Silicon.Pages
+namespace WebApp_Silicon.Pages;
+
+public class SignUpModel : PageModel
 {
-    public class SignUpModel : PageModel
+    [BindProperty]
+    public SignUpFormModel Form { get; set; } = new SignUpFormModel();
+
+
+    public void OnGet()
     {
-        public void OnGet()
+    }
+
+    
+    
+    
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
         {
+            return Page();
         }
+
+        return RedirectToPage("/index");
     }
 }
