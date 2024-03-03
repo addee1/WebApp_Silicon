809c0e95890724739692617d2a35fde8cc5a424d
Adam Ottosson <addeecsgo@gmail.com>
2024-03-03 19:23:54 +0100

    SignIn validation done

---
+++ WebApp_Silicon/wwwroot/js/validationSignIn.js
@@ -0,0 +1,24 @@
+const formErrorHandler = (element, validationResult) => {
+    let spanElement = document.querySelector(`[data-valmsg-for="${element.name}"]`)
+
+    if (validationResult) {
+        element.classList.remove('input-validation-error')
+        spanElement.classList.remove('field-validation-error')
+        spanElement.classList.add('field-validation-valid')
+        spanElement.innerHTML = ''
+
+    }
+    else {
+        element.classList.add('input-validation-error')
+        spanElement.classList.add('field-validation-error')
+        spanElement.classList.remove('field-validation-valid')
+        spanElement.innerHTML = element.dataset.valRequired
+
+    }
+}
+
+const emailValidator = (element) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    formErrorHandler(element, regEx.test(element.value))
+}
+
